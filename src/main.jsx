fbea1ca580603112029695a3100043b395cc795e
Isaac G Mwithiga <bluetopi5555@gmail.com>
2026-09-14 11:45:11 -0500

    feat: add standalone newsroom entrypoint

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IntelligenceNewsroom from "./IntelligenceNewsroom.jsx";
+import "./intelligence-newsroom.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <IntelligenceNewsroom />
+  </React.StrictMode>
+);
